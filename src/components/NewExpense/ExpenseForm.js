15a864ca03d84d20a1968640f7bbf8d26e978949
Nathan Pabst <Nathan.Pabst@ey.com>
2021-05-13 07:40:05 -0500

    module 57: adding two-way binding complete.

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -6,7 +6,7 @@ const ExpenseForm = () => {
   const [enteredTitle, setEnteredTitle] = useState('');
   const [enteredAmount, setEnteredAmount] = useState('');
   const [enteredDate, setEnteredDate] = useState('');
-  
+
   // code for option 2 seen below...primarily used when you also depend upon the previous state
   // const [userInput, setUserInput] = useState({
   //   enteredTitle: '',
@@ -32,14 +32,17 @@ const ExpenseForm = () => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    
+
     const expenseData = {
       title: enteredTitle,
       amount: enteredAmount,
-      date: new Date(enteredDate)
+      date: new Date(enteredDate),
     };
 
-    console.log(expenseData); 
+    console.log(expenseData);
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
   };
 
   return (
@@ -47,7 +50,11 @@ const ExpenseForm = () => {
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
-          <input type='text' onChange={titleChangeHandler} />
+          <input
+            type='text'
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className='new-expense__control'>
           <label>Amount</label>
@@ -55,6 +62,7 @@ const ExpenseForm = () => {
             type='number'
             min='0.01'
             step='0.01'
+            value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -64,6 +72,7 @@ const ExpenseForm = () => {
             type='date'
             min='2021-04-02'
             max='2024-12-31'
+            value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
