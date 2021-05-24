8d10eabc6d97749a5dab505ba0ed7c702eddaf2b
Nathan Pabst <36492613+nathanpabst@users.noreply.github.com>
2021-05-24 17:08:01 -0500

    Np section4 (#3)
    * section 46 complete. adding a button and clickHandler function.
    
    * module 47 complete. Module 48 State WIP.
    
    * module 48 State WIP.
    
    * module 48 Working with State complete.
    
    * notes on module 49 WIP.
    
    * module 49 notes complete.
    
    * module 51 adding form inputs complete.
    
    * module 52: listening to user input completed.
    
    * module 53: working with multiple states complete.
    
    * module 54: using one state instead complete.
    
    * module 55: updating state that depends on prev state section complete.
    
    * module 56: handling form submission complete.
    
    * module 57: adding two-way binding complete.
    
    * module 58. step one for passing data from a child to parent component.
    
    * module 58: child to parent component communication section complete with notes.
    
    * module 59: lifting the state up notes complete.
    
    * assignment 2. WIP.
    
    * assignment 2: changes to fix the render errors.
    
    * assignment WIP.
    
    * step one for assignment two complete and returning the selected year in the console.
    
    * assignment two step two complete.
    
    * assignment 2 step 3 and cleanup completed.
    
    * adding notes on the steps required for assignment 2.
    
    * adding notes on module 60 and removing unused code.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
 const App = () => {
   const expenses = [
@@ -25,12 +26,17 @@ const App = () => {
     },
   ];
 
+  const addExpenseHandler = (expense) => {
+    console.log('in app.js');
+    console.log(expense);
+  };
+
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+
+import './ExpenseForm.css';
+
+const ExpenseForm = (props) => {
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
+
+  // code for option 2 seen below...primarily used when you also depend upon the previous state
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: '',
+  //   enteredAmount: '',
+  //   enteredDate: '',
+  // });
+
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+    // option 2...
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredTitle: event.target.value };
+    //  });
+  };
+
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+    //console.log(expenseData); --replaced by the following line of code. 
+    props.onSaveExpenseData(expenseData);
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className='new-expense__controls'>
+        <div className='new-expense__control'>
+          <label>Title</label>
+          <input
+            type='text'
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
+        </div>
+        <div className='new-expense__control'>
+          <label>Amount</label>
+          <input
+            type='number'
+            min='0.01'
+            step='0.01'
+            value={enteredAmount}
+            onChange={amountChangeHandler}
+          />
+        </div>
+        <div className='new-expense__control'>
+          <label>Date</label>
+          <input
+            type='date'
+            min='2021-04-02'
+            max='2024-12-31'
+            value={enteredDate}
+            onChange={dateChangeHandler}
+          />
+        </div>
+      </div>
+      <div className='new-expense__actions'>
+        <button type='submit'>Add Expense</button>
+      </div>
+    </form>
+  );
+};
+
+export default ExpenseForm;
